96a7e24e23fe8fd24c3c358ebd503b8d6fc8ceb6
Hassan Ali Arshad <hassanali.arshad98@users.noreply.github.com>
2021-03-03 23:30:24 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Dinner from './dinner.js'
 function App() {
   return (
     <div className="App">
-      <Dinner StudentName="Hassan Tanoli"/>
+      <Dinner StudentName="Hassan Tanoli" StudentAge="23"/>
+      <hr />
+      <Dinner StudentName="Hassan Khan" StudentAge="22"/>
+      <hr />
+      <Dinner StudentName="Hassan Cypher" StudentAge="21"/>
     </div>
   );
 }
--- src/dinner.js
+++ src/dinner.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Dinner(props){
     return (
-        <h1>My Name is {props.StudentName}</h1>
+        <div>
+            <h1>My Name is {props.StudentName} .</h1>
+            <h1>I am {props.StudentAge} years old.</h1>
+        </div>
     )
 }
 export default Dinner;
